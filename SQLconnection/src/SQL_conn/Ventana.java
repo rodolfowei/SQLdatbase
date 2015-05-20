5def3b71c2e09c6db5e3ea676fc44eae5be32fe5
rodolfowei <crm_b@hotmail.com>
2015-05-20 19:57:48 +0200

    Implementados todos los botones y funciones

--- SQLconnection/src/SQL_conn/Ventana.java
+++ SQLconnection/src/SQL_conn/Ventana.java
@@ -3,15 +3,22 @@ package SQL_conn;
 import java.awt.Image;
 import java.awt.Toolkit;
 
+import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 import javax.swing.JButton;
 
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.awt.image.RenderedImage;
+import java.io.File;
 import java.util.ArrayList;
+import java.util.StringTokenizer;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import javax.swing.JLabel;
 import javax.swing.SwingConstants;
@@ -20,6 +27,10 @@ import javax.swing.JPanel;
 public class Ventana extends javax.swing.JFrame {
 
 	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	ArrayList<Imagen> imagenes;
 	String ruta,nombre;
 	ProductsDB bd;
@@ -29,15 +40,19 @@ public class Ventana extends javax.swing.JFrame {
 	
 	private JFrame frame;
 	private JTextField textFieldruta;
-	private JButton BotonAbrirDB;
-	
+		
 	private JPanel panel1;
-	private JLabel jLabelpreview1;
 	private JPanel subpanel1;
 	private JPanel subpanel2;
-	private JButton btnprevious;
-	private JButton btnNext;
+	
+	private JLabel jLabelpreview1;
 	private JLabel jlabelpreview2;
+	
+	private JButton BotonAbrirDB;   //boton1
+	private JButton btnprevious;    //boton4
+	private JButton btnNext;        //boton3
+	private JButton btnGuardar;     //boton2
+	private JButton btnDescargar;   //boton5
 
 
 	public Ventana() {
@@ -74,8 +89,17 @@ public class Ventana extends javax.swing.JFrame {
 		textFieldruta.setColumns(10);
 		
 		BotonAbrirDB = new JButton("Abrir");
-		BotonAbrirDB.setBounds(422, 29, 89, 23);
+		BotonAbrirDB.setBounds(436, 29, 89, 23);
 		subpanel1.add(BotonAbrirDB);
+		
+		btnGuardar = new JButton("Guardar");
+		btnGuardar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent evt) {
+				BotonGuardarDBActionPerformed(evt);
+			}
+		});
+		btnGuardar.setBounds(436, 113, 89, 23);
+		subpanel1.add(btnGuardar);
 		
 		subpanel2 = new JPanel();
 		subpanel2.setBounds(0, 211, 535, 124);
@@ -83,10 +107,20 @@ public class Ventana extends javax.swing.JFrame {
 		subpanel2.setLayout(null);
 		
 		btnprevious = new JButton("Previous");
+		btnprevious.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent evt) {
+				BotonPreviousDBActionPerformed(evt);
+			}
+		});
 		btnprevious.setBounds(29, 90, 89, 23);
 		subpanel2.add(btnprevious);
 		
 		btnNext = new JButton("Next");
+		btnNext.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent evt) {
+				BotonNextDBActionPerformed(evt);
+			}
+		});
 		btnNext.setBounds(411, 90, 89, 23);
 		subpanel2.add(btnNext);
 		
@@ -94,6 +128,15 @@ public class Ventana extends javax.swing.JFrame {
 		jlabelpreview2.setHorizontalAlignment(SwingConstants.CENTER);
 		jlabelpreview2.setBounds(192, 11, 146, 102);
 		subpanel2.add(jlabelpreview2);
+		
+		btnDescargar = new JButton("Descargar");
+		btnDescargar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent evt) {
+				BotonDescargarDBActionPerformed(evt);
+			}
+		});
+		btnDescargar.setBounds(436, 11, 89, 23);
+		subpanel2.add(btnDescargar);
 		
 		
 		BotonAbrirDB.addActionListener(new ActionListener() {
@@ -122,5 +165,66 @@ public class Ventana extends javax.swing.JFrame {
 			}
 		}
 		
+	}
+	
+	private void BotonGuardarDBActionPerformed(ActionEvent evt){
+		if(!ruta.equals("")){
+			if(bd.guardarImagen(ruta, nombre)){
+				JOptionPane.showMessageDialog(null, "Imagen guardada Exitosamente en la base de datos", "Hecho", JOptionPane.INFORMATION_MESSAGE);
+				textFieldruta.setText("");
+				jLabelpreview1.setIcon(null);
+				refrescarCarrusel(0);
+			}else{
+				JOptionPane.showMessageDialog(null, "La imagen no ha podido ser Guardada en la base de datos","Error", JOptionPane.ERROR_MESSAGE);
+			}
+		}
+		
+	}
+	
+	private void BotonDescargarDBActionPerformed(ActionEvent evt){
+		
+		System.out.println(imagenes.get(contador).getNombre());
+		try{
+			final JFileChooser elegirCarpeta = new JFileChooser();
+			elegirCarpeta.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+			int o = elegirCarpeta.showSaveDialog(elegirCarpeta); //Don't know yet if here goes elegirCarpeta or this 
+			if(o == JFileChooser.APPROVE_OPTION){
+				String path = elegirCarpeta.getSelectedFile().getAbsolutePath();
+				System.out.println(path.length()+" : "+path);
+				String aux = imagenes.get(contador).getNombre();
+				StringTokenizer token = new StringTokenizer(aux,".");
+				token.nextToken();
+				String formato = token.nextToken();
+				ImageIO.write((RenderedImage) imagenes.get(contador).getImagen(), formato, new File(path+"\\"+imagenes.get(contador).getNombre()));
+				}
+		}catch(Exception ex){
+			Logger.getLogger(Ventana.class.getName()).log(Level.SEVERE, null, ex);
+		}
+	}	
+	
+	private void BotonNextDBActionPerformed(ActionEvent evt){
+		contador++;
+		refrescarCarrusel(contador);
+		
+	}
+	
+	private void BotonPreviousDBActionPerformed(ActionEvent evt){
+		if((contador-1)>=0){
+			contador--;
+			refrescarCarrusel(contador);
+		}
+	}
+	
+	private void refrescarCarrusel(int p){
+		imagenes = bd.getImages();
+		if(p < imagenes.size()){
+			if(imagenes.size()>0){
+				System.out.println("leyendo Imagen");
+				ImageIcon icon = new ImageIcon(imagenes.get(p).getImagen().getScaledInstance(jlabelpreview2.getWidth(), jlabelpreview2.getHeight(), Image.SCALE_DEFAULT));
+				jlabelpreview2.setText("");
+				jlabelpreview2.setIcon(icon);
+				jlabelpreview2.updateUI();
+			}
+		}
 	}
 }
