81c6c67ed7d29c1c47c1ad5238e51df2548d44c7
rodolfowei <crm_b@hotmail.com>
2015-05-20 18:07:48 +0200

    comiting the main

--- SQLconnection/src/SQL_conn/ImageconnectionApp.java
+++ SQLconnection/src/SQL_conn/ImageconnectionApp.java
@@ -4,7 +4,10 @@ public class ImageconnectionApp {
 
 	public static void main(String args[]) {
 		
-		
-		
+		Ventana win = new Ventana();
+		win.setTitle("Guardar y leer Imagenes en Mysql");
+		win.setLocationRelativeTo(null);
+		win.setResizable(false);
+		win.setVisible(true);
 	}
 }
